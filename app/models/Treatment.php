12f6d571ebb47218c8c668a3ff05e6ec6e5ba6ff
cahya <dwayudwicahya@gmail.com>
2026-07-07 13:56:51 +0800

    membuat model treatment

--- app/models/Treatment.php
+++ app/models/Treatment.php
@@ -6,7 +6,6 @@ class Treatment {
         $this->conn = $db;
     }
 
-    // Tampilkan semua layanan
     public function getAll() {
         $query = "SELECT * FROM treatments ORDER BY id DESC";
         $stmt = $this->conn->prepare($query);
@@ -14,7 +13,6 @@ class Treatment {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // Tambah layanan baru
     public function add(string $nama, int $harga, int $durasi) {
         $query = "INSERT INTO treatments (nama_layanan, harga, durasi_menit) VALUES (?, ?, ?)";
         $stmt = $this->conn->prepare($query);
@@ -25,7 +23,6 @@ class Treatment {
         ]);
     }
 
-    // Hapus layanan
     public function delete(int $id) {
         $query = "DELETE FROM treatments WHERE id = ?";
         $stmt = $this->conn->prepare($query);
